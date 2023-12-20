274291fa1fd0b29363e2b8ea9ef53dbc40b95a93
Robins <robinmgk077@gmail.com>
2023-12-20 16:46:56 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Code your solution here
+const drivers = ["Bobby", "Sammy", "Sally", "Annette", "Sarah", "Bobby"];
+
+function findMatching(drivers, name) {
+  return drivers.filter(function (driver) {
+    return driver.toLowerCase() === name.toLowerCase();
+  });
+}
+
+function fuzzyMatch(drivers, check) {
+  return drivers.filter(function (driver) {
+    return driver.slice(0, check.length) === check;
+  });
+}
+
+const drive = [
+  {
+    name: "Bobby",
+    hometown: "Pittsburgh",
+  },
+  {
+    name: "Sammy",
+    hometown: "New York",
+  },
+  {
+    name: "Sally",
+    hometown: "Cleveland",
+  },
+  {
+    name: "Annette",
+    hometown: "Los Angeles",
+  },
+  {
+    name: "Bobby",
+    hometown: "Tampa Bay",
+  },
+];
+
+function matchName(drive, argumt) {
+  return drive.filter(function (driver) {
+    return driver.name === argumt;
+  });
+}
